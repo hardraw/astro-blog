b8aba1a40b10fc7b6841167cd71a66ae255ef522
Harby Robayo <harby250@gmail.com>
2023-03-19 16:06:26 -0500

    add netlify cms config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,38 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import NetlifyCMS from 'astro-netlify-cms';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(),
+		NetlifyCMS({
+			config: {
+			  backend: {
+				name: 'git-gateway',
+				branch: 'main',
+			  },
+			  collections: [
+				// Define a blog post collection
+				{
+					name: 'Blog',
+					label: 'Entrada',
+					folder: 'src/content/blog',
+					create: true,
+					delete: true,
+					fields: [
+					  { name: 'title', widget: 'string', label: 'Post Title' },
+					  { name: 'description', widget: 'string', label: 'Resumen' },
+					  { name: 'body', widget: 'markdown', label: 'Post Body' },
+					  { name: 'pubDate', widget: 'datetime', label: 'Fecha de publicacion' },
+					  { name: 'heroImage', widget: 'image', label: 'Imagen destacada' },
+					],
+				  },
+			  ],
+			},
+		  }),
+	],
+
 });
---
+++ src/content/blog/esta-es-otra-entrada-del-post.md
@@ -0,0 +1,7 @@
+---
+title: Esta es otra entrada del post
+description: buenas
+pubDate: 2023-03-19T17:41:32.396Z
+heroImage: /placeholder-about.jpg
+---
+lorem lorem
